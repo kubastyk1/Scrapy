314093d2a7c17bb800ef2edb9ac356f658efdba1
Nerond32 <nerond32@gmail.com>
2019-12-15 11:14:52 +0100

    List of people saved to file, now fullcast

--- imdb/spiders/main.py
+++ imdb/spiders/main.py
@@ -17,20 +17,27 @@ class QuotesSpider(scrapy.Spider):
         links = []
         for link in response.css('a::attr(href)').extract():
             if "/title/tt" in link:
-                links.append(link)
+                links.append(link+'fullcredits')
 
         for href in links:
             yield response.follow(href, self.parse_actors)
 
     def parse_actors(self, response):
-        actorNames = response.xpath('//div[@id="titleCast"]//a/text()').getall()
-        for actor in actorNames[1:-5:2]:
-            global actorDict
-            self.log(actorDict)
-
+        actorNames = response.xpath('//a[contains(@href,"/name/nm")]/text()').getall()
+        for actor in actorNames:
+            global actorDict;
+            #self.log(actorDict)
             if actor in actorDict:
-                actorDict[actor] += 1
+                actorDict[actor] += 1;
             else:
-                actorDict[actor] = 1
-
+                actorDict[actor] = 1;
+        actorList = [];
+        for key, value in actorDict.items():
+            tmp = [key, value]
+            actorList.append(tmp);
+        actorList.sort(key=lambda x: x[1]);
+        f = open("top_actors.txt", "w");
+        for actor in actorList[::-1]:
+            f.write('{0: <16}'.format(" ".join(actor[0].split())) + " :" + str(actor[1])+'\n')
+        f.close();
 # scrapy crawl quotes
